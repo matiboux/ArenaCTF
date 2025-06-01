689f8ff3360804cc99825381909b245961f8b03b
Matiboux <matiboux@gmail.com>
2025-06-01 21:40:13 +0200

    Add ArenaFlagDropEvent

---
+++ src/main/java/org/battleplugins/arena/ctf/event/ArenaFlagDropEvent.java
@@ -0,0 +1,36 @@
+package org.battleplugins.arena.ctf.event;
+
+import org.battleplugins.arena.ArenaPlayer;
+import org.battleplugins.arena.ctf.ArenaCtf;
+import org.battleplugins.arena.event.EventTrigger;
+import org.battleplugins.arena.event.player.BukkitArenaPlayerEvent;
+import org.battleplugins.arena.resolver.Resolver;
+import org.battleplugins.arena.resolver.ResolverKey;
+import org.battleplugins.arena.resolver.ResolverProvider;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+@EventTrigger("on-flag-drop")
+public class ArenaFlagDropEvent extends BukkitArenaPlayerEvent {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public ArenaFlagDropEvent(@NotNull ArenaPlayer player) {
+        super(player.getArena(), player);
+    }
+
+    @Override
+    public Resolver resolve() {
+        return super.resolve().toBuilder()
+                .build();
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
