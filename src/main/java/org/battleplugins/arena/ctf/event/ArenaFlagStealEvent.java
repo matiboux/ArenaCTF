79287317d489d450910640206e7524d6ef43ecc7
Matiboux <matiboux@gmail.com>
2025-06-02 04:51:01 +0200

    Add ArenaFlagStealEvent

---
+++ src/main/java/org/battleplugins/arena/ctf/event/ArenaFlagStealEvent.java
@@ -0,0 +1,30 @@
+package org.battleplugins.arena.ctf.event;
+
+import org.battleplugins.arena.ArenaPlayer;
+import org.battleplugins.arena.ctf.ArenaCtf;
+import org.battleplugins.arena.event.EventTrigger;
+import org.battleplugins.arena.event.player.BukkitArenaPlayerEvent;
+import org.battleplugins.arena.resolver.Resolver;
+import org.battleplugins.arena.resolver.ResolverKey;
+import org.battleplugins.arena.resolver.ResolverProvider;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+@EventTrigger("on-flag-steal")
+public class ArenaFlagStealEvent extends BukkitArenaPlayerEvent {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    public ArenaFlagStealEvent(@NotNull ArenaPlayer player) {
+        super(player.getArena(), player);
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
